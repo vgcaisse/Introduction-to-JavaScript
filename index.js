0b3ba9feea2b6f89a5bc52d4e68b6eeca269fcac
Visnu <vgcaisse@gmail.com>
2021-11-02 16:58:40 -0400

    finished the task 3 and 4 and fixed a small typo

--- index.js
+++ index.js
@@ -92,8 +92,7 @@ Do the following:
 */
 
 function dogYears(age){
-  let humanYears = dogYears * 7;
-  return dogYears;
+  return age * 7;
 }
 
 console.log(dogYears(8));
@@ -167,10 +166,34 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
+let computer = Math.random();
+
+if (computer <= 0.3) {
+  computer = 'rock'; 
+} else if (computer >= 0.6) {
+  computer = 'paper';
+} else if (computer > 0.6) {
+  computer = 'scissors';
+}
 
 function game(user, computer){
-  /*add your code here*/
+
+  if (user === computer) {
+    return `it's a tie`;
+  } else if (user === 'scissors' && computer === 'paper') {
+    return `you win!`;
+  } else if (user === 'paper' && computer === 'rock') {
+    return `you win!`;
+  } else if (user === 'rock' && computer === 'scissors') {
+    return `you win!`;
+  } else if (user === 'scissors' && computer === 'paper') {
+    return `you win!`;
+  } else {
+    return `you lose!`;
+  }  
 }
+
+console.log(game('rock', computer));
 
 
 
